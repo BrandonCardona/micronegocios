09834859cf6d484f6d88f955358ede646e7f49a0
Brandon Cardona <cardonamartinezb@gmail.com>
2024-11-21 11:34:45 -0500

    61 Commit

--- app.py
+++ app.py
@@ -156,9 +156,9 @@ elif st.session_state["navbar_selection"] == "Métodos":
 
             elif  metodo_confirmado_secundario == "SVM (Super Vector Machines)":
                 st.title("Resultados del Modelo Super Vector Machines")
-                st.markdown(f"<h2 style='font-size: 24px;'>**Accuracy**: {accuracy:.8f}</h2>", unsafe_allow_html=True)
-                st.markdown(f"<h2 style='font-size: 24px;'>**Recall**: {recall:.8f}</h2>", unsafe_allow_html=True)
-                st.markdown(f"<h2 style='font-size: 24px;'>**F1-score**: {f1_score:.8f}</h2>", unsafe_allow_html=True)
+                st.markdown(f"<h2 style='font-size: 24px;'>Accuracy: {accuracy:.8f}</h2>")
+                st.markdown(f"<h2 style='font-size: 24px;'>Recall: {recall:.8f}</h2>")
+                st.markdown(f"<h2 style='font-size: 24px;'>F1-score: {f1_score:.8f}</h2>")
 
                 st.write("### Matriz de Confusión")
                 plt.figure(figsize=(8, 6))
