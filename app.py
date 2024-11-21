a31e904dd4a1b8c7ceb6d9d71c39f35e24a5d76f
Brandon Cardona <cardonamartinezb@gmail.com>
2024-11-21 18:52:44 -0500

    69 Commit

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ score_dbsacn_c = DBSCAN_results['score_AGclustering_c']
 score_dbsacn_d = DBSCAN_results['score_AGclustering_d']
 
 eps_values = var_pkl['eps_values']
-n_clusters = var_pkl['n_clusters']
+n_clusterss = var_pkl['n_clusters']
 optimal_clusters = var_pkl['optimal_clusters']
 optimal_eps = var_pkl['optimal_eps']
 
@@ -348,9 +348,9 @@ elif st.session_state["navbar_selection"] == "Métodos":
 
             elif metodo_confirmado_secundario == "DB-Scan":
                 st.subheader("Métricas para DB-Scan")
-                
+
                 plt.figure(figsize=(10, 6))
-                plt.plot(eps_values, n_clusters, marker='o', color='b')
+                plt.plot(eps_values, n_clusterss, marker='o', color='b')
                 plt.title('Número de Clusters por DBSCAN en función de Epsilon (eps)')
                 plt.xlabel('Valor de Epsilon (eps)')
                 plt.ylabel('Número de Clusters')
