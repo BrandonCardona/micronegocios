1f13e3ad11aa6947e75fbd92bf20cfb44d6e225c
Brandon Cardona <cardonamartinezb@gmail.com>
2024-11-21 23:01:51 -0500

    69 Commit

--- app.py
+++ app.py
@@ -452,7 +452,7 @@ elif st.session_state["navbar_selection"] == "Predicciones":
 
                     x_in = registro_usuario_escalado.values.reshape(1, -1)
                     predicts_svm = model_svm.predict(x_in)
-                    st.markdown(f"<h2 style='font-size: 24px;'>Predicción SVM: {predicts_svm[0]:.8f}</h2>", unsafe_allow_html=True)
+                    st.markdown(f"<h2 style='font-size: 24px;'>Predicción SVM: {predicts_svm[0]}</h2>", unsafe_allow_html=True)
 
 
                 elif MetodoSupervisadoSeleccionado == 'Naive Bayes':
@@ -460,14 +460,14 @@ elif st.session_state["navbar_selection"] == "Predicciones":
 
                     x2_in = registro_usuario_escalado.values.reshape(1, -1)
                     predicts_nb = model_nb.predict(x2_in)
-                    st.markdown(f"<h2 style='font-size: 24px;'>Predicción Naive Bayes: {predicts_nb[0]:.8f}</h2>", unsafe_allow_html=True)
+                    st.markdown(f"<h2 style='font-size: 24px;'>Predicción SVM: {predicts_nb[0]}</h2>", unsafe_allow_html=True)
                 
                 elif MetodoSupervisadoSeleccionado == 'KNN':
                     st.subheader('KNN')
 
                     x3_in = registro_usuario_escalado.values.reshape(1, -1)
                     predicts_knn = model_knn.predict(x3_in)
-                    st.markdown(f"<h2 style='font-size: 24px;'>Predicción KNN: {predicts_knn[0]:.8f}</h2>", unsafe_allow_html=True)
+                    st.markdown(f"<h2 style='font-size: 24px;'>Predicción SVM: {predicts_knn[0]}</h2>", unsafe_allow_html=True)
 
             except ValueError as e:
                 st.error(f"Error en la transformación: {e}")
